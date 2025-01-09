1e036ce881257581997c27d5663c333baadcbdb9
abdullah <44318011+fx2y@users.noreply.github.com>
2025-01-09 11:44:26 +0700

    Frontend Testing and Docker Configurations

---
+++ frontend/src/components/__tests__/ConfigForm.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import ConfigForm from '../ConfigForm';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { SessionProvider } from '../../contexts/SessionContext';
+
+const server = setupServer(
+  rest.post('http://localhost:8000/config', (req, res, ctx) => {
+    return res(ctx.json({ session_id: 'mock_session_id', token: 'mock_token' }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('renders ConfigForm and submits data', async () => {
+    const mockSetSessionData = jest.fn();
+    render(
+        <SessionProvider value={{ setSessionData: mockSetSessionData }}>
+            <ConfigForm />
+        </SessionProvider>
+    );
+
+    userEvent.type(screen.getByLabelText(/project id/i), 'test_project_id');
+    userEvent.selectOptions(screen.getByLabelText(/region/i), 'us-central1');
+    userEvent.selectOptions(screen.getByLabelText(/voice/i), 'Puck');
+    userEvent.type(screen.getByLabelText(/api key/i), 'test_api_key');
+
+    fireEvent.click(screen.getByRole('button', { name: /submit/i }));
+
+    await waitFor(() => {
+        expect(mockSetSessionData).toHaveBeenCalledWith({ session_id: 'mock_session_id', token: 'mock_token' });
+    });
+});
---
+++ frontend/src/setupTests.js
@@ -0,0 +1,2 @@
+// Optional: Any global test setup you might need (e.g., mocking libraries)
+import '@testing-library/jest-dom/extend-expect';
